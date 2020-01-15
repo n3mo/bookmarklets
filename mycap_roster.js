fb0f573558e043284ffd269488dbadbe410eea6c
Nicholas M. VanHorn <nvanhorn@app-saitama.capital.edu>
2020-01-15 16:00:50 -0500

    Function-ized

--- mycap_roster.js
+++ mycap_roster.js
@@ -2,15 +2,19 @@
 // display to include the number of students actually enrolled in the
 // class
 
-// Grab nodes containing student info
-var names = document.querySelectorAll("[id^=roster_studentname]");
-var N_enrolled = names.length;
+function update_roster() {
+    // Grab nodes containing student info
+    var names = document.querySelectorAll("[id^=roster_studentname]");
+    var N_enrolled = names.length;
 
-// Default text (shows how many seats are available, NOT how many are
-// enrolled. So if your course is OVER capacity, this info cannot
-// reveal this)
-var availability = document.getElementById("section-seat-availability").innerHTML;
+    // Default text (shows how many seats are available, NOT how many are
+    // enrolled. So if your course is OVER capacity, this info cannot
+    // reveal this)
+    var availability = document.getElementById("section-seat-availability").innerHTML;
 
-// Update "seats available" to include
-document.getElementById("section-seat-availability")
-    .innerHTML = availability + " (" + N_enrolled + " enrolled)";
+    // Update "seats available" to include
+    document.getElementById("section-seat-availability")
+	.innerHTML = availability + " (" + N_enrolled + " enrolled)";
+}
+
+update_roster();
